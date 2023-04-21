75922abc74a7c123aa0c35e7135f80d0a7a9bd04
Sathish Obili <sathish.obili@gmail.com>
2023-04-21 08:21:13 +0530

    base code

---
+++ apps/client/src/App.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [gretting, setGretting] = useState('')
+
+  useEffect(() => {
+    fetch('/api')
+    .then((res) => res.text())
+    .then(setGretting);
+  }, [])
+  
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      {gretting}
+    </div>
+  )
+}
+
+export default App
